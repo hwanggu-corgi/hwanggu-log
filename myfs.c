40f6dfbd1f92ffafe1e40315193bb004ac4c2912
Hyungmo Gu <guhyungm7@gmail.com>
2020-10-02 01:08:16 +0900

    feat: Add progress

--- myfs.c
+++ myfs.c
@@ -1,3 +1,7 @@
+// TODO LIST
+// []: Setup tests
+// []: Write Pathlookup function
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -12,21 +16,21 @@
 #define MAXLINE 128
 #define MAXPATH 128
 
-// Inode table 
+// Inode table
 struct inode itable[MAX_INODES];
 
 /* The data blocks are implemented as an 2d array of dir_entry structs.
- * You can think of it as an array of blocks where each block is an array of 
+ * You can think of it as an array of blocks where each block is an array of
  * directory entries.
  */
 struct dir_entry blocks[MAX_BLOCKS][MAX_DIRS];
 
 /* Populate itable and blocks from the open file handle fp
- * You can expect that the input file data format is correct 
+ * You can expect that the input file data format is correct
  *
  * Note: this is kind of a hack to find a quick way to represent
  * the file system and read it into memory.  It is not elegant or particularly
- * readable. It also takes advantage of the fact that global variables are 
+ * readable. It also takes advantage of the fact that global variables are
  * initialized to 0.
  */
 void populate_fs( FILE *fp) {
@@ -38,12 +42,12 @@ void populate_fs( FILE *fp) {
     }
 
     // read the blocks
-    char line[MAXLINE]; 
+    char line[MAXLINE];
     i = 0;  // block index
     int j = 0;
     while((fgets(line, MAXLINE, fp)) != NULL) {
         line[strlen(line) - 1] = '\0';
-        
+
         if(line[0] == '\0')  {
             i++;
             j = 0;
@@ -69,7 +73,7 @@ int path_lookup(char *path) {
     if(path[0] != '/') {
         fprintf(stderr, "Not an absolute path\n");
         return -1;
-    } 
+    }
 
 	// TODO: complete this function and any helper functions
 
@@ -83,8 +87,8 @@ int main(int argc, char **argv) {
         fprintf(stderr, "USAGE: myfs fs_file path_to_search\n");
         exit(1);
     }
-    /* first argument is a path to a text file containing a 
-     * a text representation of a file system.  
+    /* first argument is a path to a text file containing a
+     * a text representation of a file system.
      * You can expect that lines are terminated with \n.
      */
     FILE *fp = fopen(argv[1], "r");
